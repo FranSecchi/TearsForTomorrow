67d55f5227bd3cf615c2f8a78f417cb62a770ef2
Carlitusc48 <88926226+Carlitusc48@users.noreply.github.com>
2024-03-03 23:29:27 +0100

    Estatua_p1

--- Assets/Scripts/Parameter.cs
+++ Assets/Scripts/Parameter.cs
@@ -7,7 +7,7 @@ public static class Parameter
     public const string SAVEFILE_NAME = "_GameData.txt";
     private const string STARTMENU_SCENE = "MainMenu";
     private const string PRESENT_SCENE = "PresentScene";
-    private const string COFRE_KEY = "CofreKey";
+    private const string COFRE_KEY = "nStatue";
     private const string ELEVATOR_BTN = "nElevatorBtn";
     private const string DOOR_KEY = "nKey";
     private const string POLAROID_KEY = "nPolaroid";
