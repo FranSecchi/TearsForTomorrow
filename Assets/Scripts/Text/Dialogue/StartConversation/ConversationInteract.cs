d593bb91a1fa3d10294864ca7544337aa7f49e21
franciscoSecchiTarres <82807613+FranSecchi@users.noreply.github.com>
2024-03-03 14:39:04 +0100

    Dialegs

--- Assets/Scripts/Text/Dialogue/StartConversation/ConversationInteract.cs
+++ Assets/Scripts/Text/Dialogue/StartConversation/ConversationInteract.cs
@@ -9,6 +9,7 @@ public class ConversationInteract : MonoBehaviour,Interactuable
     public DialogueManager DialogueManager;
     public Transform StandPosition;
     public GameObject talker;
+    public Conversation Repeat;
     public Transform getStandPosition()
     {
         return StandPosition;
@@ -19,7 +20,13 @@ public class ConversationInteract : MonoBehaviour,Interactuable
         if (activate)
             DialogueManager.StartConversation(NewConversation, talker);
         else
+        {
             DialogueManager.FinishConversation();
+            if (Repeat != null)
+            {
+                NewConversation = Repeat;
+            }
+        }
         PlayerAnimation.instance.Talk(activate);
         talker.GetComponent<Animator>().SetBool("Talk", activate);
         switch (talker.name){
